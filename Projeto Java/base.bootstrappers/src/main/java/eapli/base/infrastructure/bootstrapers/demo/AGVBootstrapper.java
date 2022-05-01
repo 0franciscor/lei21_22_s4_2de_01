be9cbc8b6b133b9d9606f640e1a17c74b5dd57fc
pedroccelestino <1201382@isep.ipp.pt>
2022-05-01 02:06:05 +0100

    #11 - Bootstrap Update

---
+++ Projeto Java/base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/AGVBootstrapper.java
@@ -0,0 +1,31 @@
+package eapli.base.infrastructure.bootstrapers.demo;
+
+import eapli.base.AGVManagement.Application.ConfigureAGVController;
+import eapli.base.warehouse.domain.AGVDock;
+import eapli.framework.actions.Action;
+
+import java.util.List;
+
+public class AGVBootstrapper extends BaseDemoBootstrapper implements Action {
+
+    private final ConfigureAGVController configureAGVController = new ConfigureAGVController();
+
+    @Override
+    public boolean execute() {
+        createAGV();
+        return true;
+    }
+
+    private boolean createAGV(){
+        List<AGVDock> list = (List<AGVDock>) configureAGVController.configureAGVDock();
+        AGVDock dock = list.get(0);
+
+        configureAGVController.configureAGV("12345678","abcdefg","ASD56S",30.0,35.0,5.0,"x",dock,"FREE","123");
+        return true;
+    }
+
+
+
+
+
+}
