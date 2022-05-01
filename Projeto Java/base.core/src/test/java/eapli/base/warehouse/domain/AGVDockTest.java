3434cc52429ea4e6f688b4e09122de196362ee3a
franciscoredol <1201239@isep.ipp.pt>
2022-05-01 05:53:38 +0100

    #8 - Requirements Design Readme Updated.

---
+++ Projeto Java/base.core/src/test/java/eapli/base/warehouse/domain/AGVDockTest.java
@@ -0,0 +1,20 @@
+package eapli.base.warehouse.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AGVDockTest {
+
+    @Test
+    void sameAs() {
+    }
+
+    @Test
+    void identity() {
+    }
+
+    @Test
+    void getId() {
+    }
+}
