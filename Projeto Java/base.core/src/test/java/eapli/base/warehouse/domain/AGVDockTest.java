28e66153c8e5084691149f44432fe2b226ff4010
franciscoredol <1201239@isep.ipp.pt>
2022-05-01 06:33:22 +0100

    #8 - Class Tests Added.

--- Projeto Java/base.core/src/test/java/eapli/base/warehouse/domain/AGVDockTest.java
+++ Projeto Java/base.core/src/test/java/eapli/base/warehouse/domain/AGVDockTest.java
@@ -8,13 +8,31 @@ class AGVDockTest {
 
     @Test
     void sameAs() {
+        AGVDock agvDock = new AGVDockBuilder().withId("1L").withWarehouse(new Warehouse()).withDepth(new Depth()).withAccessibility(new Accessibility()).withBegin(new Begin()).withEnd(new End()).build();
+        AGVDock agvDock2 = agvDock;
+
+        assertEquals(true, agvDock.sameAs(agvDock2));
+    }
+
+    @Test
+    void sameAs2() {
+        AGVDock agvDock = new AGVDockBuilder().withId("1L").withWarehouse(new Warehouse()).withDepth(new Depth()).withAccessibility(new Accessibility()).withBegin(new Begin()).withEnd(new End()).build();
+        AGVDock agvDock2 = null;
+
+        assertEquals(false, agvDock.sameAs(agvDock2));
     }
 
     @Test
     void identity() {
+        AGVDock agvDock = new AGVDockBuilder().withId("1L").withWarehouse(new Warehouse()).withDepth(new Depth()).withAccessibility(new Accessibility()).withBegin(new Begin()).withEnd(new End()).build();
+
+        assertEquals("1L", agvDock.identity());
     }
 
     @Test
     void getId() {
+        AGVDock agvDock = new AGVDockBuilder().withId("1L").withWarehouse(new Warehouse()).withDepth(new Depth()).withAccessibility(new Accessibility()).withBegin(new Begin()).withEnd(new End()).build();
+
+        assertEquals("1L", agvDock.getId());
     }
 }
