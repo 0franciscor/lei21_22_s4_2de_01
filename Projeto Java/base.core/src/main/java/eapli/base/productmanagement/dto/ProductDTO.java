07e30c3b6e7adfe55e431052a49fda3933db05dd
Rita Ariana Sobral <1201386@isep.ipp.pt>
2022-05-27 18:54:15 +0100

    #22 Criação da classe ProductDTO

---
+++ Projeto Java/base.core/src/main/java/eapli/base/productmanagement/dto/ProductDTO.java
@@ -0,0 +1,17 @@
+package eapli.base.productmanagement.dto;
+
+
+/**
+ * DTO for products.
+ *
+ * @author Rita Ariana Sobral
+ *
+ */
+public class ProductDTO {
+
+    private String productUniqueInternalCode;
+
+    private String productShortDescription;
+
+    private double price;
+}
