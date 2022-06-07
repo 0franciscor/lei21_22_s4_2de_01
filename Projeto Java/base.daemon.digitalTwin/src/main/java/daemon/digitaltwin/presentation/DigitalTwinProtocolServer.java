00f5b02d88885d2fbf033730bdb5965252348ee2
franciscoredol <1201239@isep.ipp.pt>
2022-06-07 18:54:49 +0100

    #27 - Alteração da licença MIT

--- Projeto Java/base.daemon.digitalTwin/src/main/java/daemon/digitaltwin/presentation/DigitalTwinProtocolServer.java
+++ Projeto Java/base.daemon.digitalTwin/src/main/java/daemon/digitaltwin/presentation/DigitalTwinProtocolServer.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2013-2022 the original author or authors.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package daemon.digitaltwin.presentation;
 
 import digitaltwin.tcpprotocol.server.DigitalTwinProtocolRequest;
@@ -7,23 +27,34 @@ import eapli.base.orderServer.tcp.utils.ConstantsServer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
-import java.net.ServerSocket;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.Socket;
 
+
 /**
- * Server socket for booking daemon using the CSV-based protocol.
+ * Server socket for Digital Twin Using byte arrays as the way of communication
  *
- * @author Paulo Gandra Sousa 01/06/2020
+ * @author Francisco Redol
  */
 public class DigitalTwinProtocolServer {
 
     private static final Logger LOGGER = LogManager.getLogger(DigitalTwinProtocolServer.class);
 
+    private static final String TRUSTED_STORE = "keystore.jks";
+
+    private static final String STORE_PATH = "base.daemon.digitalTwin/src/main/resources/" + TRUSTED_STORE;
+
+    private static final String KEYSTORE_PASS="password";
+
     /**
      * Client socket.
      *
-     * @author Paulo Gandra Sousa 01/06/2020
+     * @author Francisco Redol
      */
     private static class ClientHandler extends Thread {
         private Socket clientSocket;
