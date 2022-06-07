21f8c17d778d602a01ef89ed8974d3bd61dea672
franciscoredol <1201239@isep.ipp.pt>
2022-06-07 18:49:18 +0100

    #27 - Alteração do Daemon para incorporar as alterações efetuadas ao Twin

--- Projeto Java/base.daemon.agvManager/src/main/java/agvmanager/tcpprotocol/server/DashboardRequest.java
+++ Projeto Java/base.daemon.agvManager/src/main/java/agvmanager/tcpprotocol/server/DashboardRequest.java
@@ -1,20 +1,14 @@
 package agvmanager.tcpprotocol.server;
 
-import eapli.base.AGV.application.CallAGVManagerController;
-import eapli.base.AGV.application.GetAGVInformation;
+import eapli.base.AGV.application.CallDigitalTwinController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
 
 public class DashboardRequest extends AGVManagerProtocolRequest{
 
     private final String agvId;
     public DashboardRequest(String inputRequest, String agvId) {
-        super(new GetAGVInformation(), inputRequest);
+        super(new CallDigitalTwinController(), inputRequest);
         this.agvId = agvId;
     }
 
@@ -30,21 +24,8 @@ public class DashboardRequest extends AGVManagerProtocolRequest{
     }
 
     public String buildResponse(final String agvId) throws IOException {
-        InetAddress serverIP = InetAddress.getByName("localhost");
-        int port = 8891;
+        callTwinController.connectDaemon(8891);
 
-        Socket socket = new Socket(serverIP, port);
-        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        System.out.printf("Connected to {%s}\n", serverIP);
-
-        String requestedAGV = String.format("%d,%d,%d,%s", CallAGVManagerController.VERSION, CallAGVManagerController.DASHBOARD_REQUEST, agvId.length(), agvId);
-
-        output.println(requestedAGV);
-        String receivedInfo = input.readLine();
-
-        socket.close();
-
-        return receivedInfo;
+        return callTwinController.getAGVInfo(agvId);
     }
 }
