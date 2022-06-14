b760435bd2dcc0f0b281abfa16aa8e23daef5c61
franciscoredol <1201239@isep.ipp.pt>
2022-06-14 23:47:34 +0100

    #29 - Control System feito em colaboração com Rita Ariana Sobral <1201386@isep.ipp.pt>

---
+++ Projeto Java/base.core/src/main/java/eapli/base/AGV/modules/ControlSystem.java
@@ -0,0 +1,33 @@
+package eapli.base.AGV.modules;
+
+import eapli.base.AGV.domain.AGV;
+
+public class ControlSystem extends Thread{
+
+    private final Thread csThread;
+
+    private final MoveAGV moveAGV;
+
+    public ControlSystem(final AGV agv){
+        csThread = new Thread(this);
+        this.moveAGV = new MoveAGV(agv, WarehouseMovement.getWarehouseMovement());
+    }
+
+    public Thread getControlSystemThread(){
+        return csThread;
+    }
+
+    @Override
+    public synchronized void run(){
+        moveAGV.setCoordinates(17,19);
+        moveAGV.start();
+
+        try {
+            this.wait(2000); // regula a frequência de atualização do Control System
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //private
+}
