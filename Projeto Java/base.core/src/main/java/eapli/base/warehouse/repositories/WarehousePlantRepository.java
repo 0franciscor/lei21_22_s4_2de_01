e0f843de2724a938b13fc9e51c27310e5892fe63
pedroccelestino <1201382@isep.ipp.pt>
2022-06-16 23:29:00 +0100

    #29 - Creation of the method for defining the matrix according to the dimensions of a particular warehouse

--- Projeto Java/base.core/src/main/java/eapli/base/warehouse/repositories/WarehousePlantRepository.java
+++ Projeto Java/base.core/src/main/java/eapli/base/warehouse/repositories/WarehousePlantRepository.java
@@ -3,4 +3,6 @@ package eapli.base.warehouse.repositories;
 import eapli.base.warehouse.domain.WarehousePlant;
 import eapli.framework.domain.repositories.DomainRepository;
 
-public interface WarehousePlantRepository extends DomainRepository<Long, WarehousePlant> {}
+public interface WarehousePlantRepository extends DomainRepository<Long, WarehousePlant> {
+    WarehousePlant findById(String id);
+}
