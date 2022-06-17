70908a4aa0c67b7457e656eb88111ae62671e25e
franciscoredol <1201239@isep.ipp.pt>
2022-06-18 00:49:51 +0100

    #29 - AGV Class Updated to incopor

--- Projeto Java/base.core/src/main/java/eapli/base/AGV/domain/AGV.java
+++ Projeto Java/base.core/src/main/java/eapli/base/AGV/domain/AGV.java
@@ -1,6 +1,5 @@
 package eapli.base.AGV.domain;
 
-import eapli.base.AGV.modules.BMS;
 import eapli.base.AGV.modules.ControlSystem;
 import eapli.base.ordermanagement.domain.ProductOrder;
 import eapli.base.warehouse.domain.AGVDock;
@@ -213,6 +212,10 @@ public class AGV implements AggregateRoot<AGVId> {
         csThread.start();
     }
 
+    public void deactivateSensors(){
+        for(var sensor : sensorList)
+            sensor.deactivate();
+    }
 
     public AGVBattery getBattery() {
         return battery;
