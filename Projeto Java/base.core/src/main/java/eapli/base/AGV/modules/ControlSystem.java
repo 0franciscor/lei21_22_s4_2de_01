6318de7f9ef1beb795b84a54b9c7583c4f70e1ab
franciscoredol <1201239@isep.ipp.pt>
2022-06-19 03:42:11 +0100

    #29 - Control System Finished.

--- Projeto Java/base.core/src/main/java/eapli/base/AGV/modules/ControlSystem.java
+++ Projeto Java/base.core/src/main/java/eapli/base/AGV/modules/ControlSystem.java
@@ -1,6 +1,10 @@
 package eapli.base.AGV.modules;
 
 import eapli.base.AGV.domain.AGV;
+import eapli.base.AGV.domain.AGVTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ControlSystem extends Thread{
 
@@ -23,9 +27,26 @@ public class ControlSystem extends Thread{
     @Override
     public void run(){
         var taskList = agv.getAgvTask();
-        
-        synchronized (this) {
-            for (var task : taskList) {
+        List<AGVTask> oldList = new ArrayList<>();
+
+        while(true){
+            taskList = agv.getAgvTask();
+            if(oldList != taskList)
+                taskService(taskList);
+            oldList = taskList;
+            try{
+                sleep(100);
+            } catch (Exception e){
+                System.out.println("There was an error when refreshing the Control System.");
+            }
+
+        }
+
+    }
+
+    private void taskService(List<AGVTask> taskList){
+        for (var task : taskList) {
+            synchronized (this) {
                 if (task.getStatus() == 1) {
                     var array = task.getLocation().split(",");
                     moveAGV.setCoordinates(Integer.parseInt(array[0]), Integer.parseInt(array[1]));
@@ -33,7 +54,7 @@ public class ControlSystem extends Thread{
                     try {
                         wait();
                     } catch (InterruptedException e) {
-                        System.out.println("There was an error when refreshing the Control System.");
+                        System.out.println("There was an error when using the Task Service");
                     }
                 }
             }
